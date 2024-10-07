a105f7b3cf88954fae75c6d63a1768d8b3972d01
jaewan96 <139323399+jaewan96@users.noreply.github.com>
2024-10-07 17:32:16 +0900

    [Bronze V] Title: 꼬마 정민, Time: 104 ms, Memory: 14060 KB -BaekjoonHub

---
+++ Java/백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
+        String input = reader.readLine();
+        StringTokenizer st = new StringTokenizer(input);
+        
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
+        
+        System.out.println(A + B + C);
+    }
+}
