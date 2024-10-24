dcc75ae346fc5c5e11ab64bf6c3bf7a51fac284e
jaewan96 <139323399+jaewan96@users.noreply.github.com>
2024-10-24 19:38:23 +0900

    [Bronze I] Title: 진법 변환 2, Time: 112 ms, Memory: 14372 KB -BaekjoonHub

--- Java/백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
+++ Java/백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -1,18 +1,29 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
-
-        String result = Integer.toString(N, B).toUpperCase();
-        System.out.println(result);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         
+        String input = reader.readLine();
+        StringTokenizer st = new StringTokenizer(input);
+        
+        int num = Integer.parseInt(st.nextToken());
+        int base = Integer.parseInt(st.nextToken());
+        
+        StringBuilder baseNum = new StringBuilder();
+        while (num > 0) {
+            int remain = num % base;
+            num /= base;
+            if (remain > 9) {
+                baseNum.append((char) ('A' + (remain - 10)));
+            } else {
+                baseNum.append(remain);
+            }
+        }
+        baseNum.reverse();
+        System.out.print(baseNum.toString());
     }
 }
