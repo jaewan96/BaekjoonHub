44755f2836123508c3067c50f9d6140cf422214c
jaewan96 <139323399+jaewan96@users.noreply.github.com>
2024-10-27 03:19:35 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 104 ms, Memory: 14092 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(reader.readLine());
+        int row = (int) Math.pow(2, N) + 1;
+        System.out.print(row * row);
+    }
+}
