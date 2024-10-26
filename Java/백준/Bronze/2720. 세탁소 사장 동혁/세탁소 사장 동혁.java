c51541979a73cf18e8a4eeff6cedda580dbbc96a
jaewan96 <139323399+jaewan96@users.noreply.github.com>
2024-10-27 03:06:20 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 172 ms, Memory: 15108 KB -BaekjoonHub

--- Java/백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
+++ Java/백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -1,38 +1,20 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int T = Integer.parseInt(br.readLine());
-
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(reader.readLine());
+        
         for (int i = 0; i < T; i++) {
-            int coin = Integer.parseInt(br.readLine());
-
-            if (coin / 25 > 0) {
-                System.out.print(coin / 25 + " ");
-                coin = coin % 25;
-            } else {
-                System.out.print(0 + " ");
-            }
-
-            if (coin / 10 > 0) {
-                System.out.print(coin / 10 + " ");
-                coin = coin % 10;
-            } else {
-                System.out.print(0 + " ");
-            }
-
-            if (coin / 5 > 0) {
-                System.out.print(coin / 5 + " ");
-                coin = coin % 5;
-            } else {
-                System.out.print(0 + " ");
-            }
-            System.out.println(coin);
+            int c = Integer.parseInt(reader.readLine());
+            System.out.print(c / 25);
+            c %= 25;
+            System.out.print(" " + (c / 10));
+            c %= 10;
+            System.out.print(" " + (c / 5));
+            System.out.println(" " + (c % 5));
         }
     }
 }
