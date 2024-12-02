c8884476a4fac46093d315649d8d23a6c2b7591b
jaewan96 <139323399+jaewan96@users.noreply.github.com>
2024-12-02 19:03:58 +0900

    [Silver V] Title: 최소공배수, Time: 108 ms, Memory: 14228 KB -BaekjoonHub

---
+++ Java/백준/Silver/13241. 최소공배수/최소공배수.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(reader.readLine());
+        long A = Integer.parseInt(st.nextToken());
+        long B = Integer.parseInt(st.nextToken());
+
+        long num = gcd(A, B);
+        System.out.println(A * B / num);
+    }
+
+    public static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+}
+
