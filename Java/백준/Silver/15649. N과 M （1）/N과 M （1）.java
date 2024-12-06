eee16b5952f873419ccedf9c34d9312006f2f8cd
jaewan96 <139323399+jaewan96@users.noreply.github.com>
2024-12-06 18:36:17 +0900

    [Silver III] Title: N과 M (1), Time: 356 ms, Memory: 39976 KB -BaekjoonHub

---
+++ Java/백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static StringBuilder result = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(reader.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        List<Integer> lst = new ArrayList<>();
+        def(N, M, lst);
+        System.out.println(result);
+    }
+
+    public static void def(int n, int m, List<Integer> lst) {
+        if (lst.size() == m) {
+            for (int i = 0; i < m; i++) {
+                result.append(lst.get(i)).append(" ");
+            }
+            result.append("\n");
+            return;
+        }
+        for (int i = 1; i < n+1; i++) {
+            if (!lst.contains(i)) {
+                lst.add(i);
+                def(n, m, lst);
+                lst.remove(lst.size()-1);
+            }
+        }
+    }
+}
