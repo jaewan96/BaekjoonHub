e62bf1c7647ed3dab648a47e9a163e7b9113f7ea
jaewan96 <139323399+jaewan96@users.noreply.github.com>
2025-04-10 09:45:51 +0900

    [Silver III] Title: 블로그, Time: 312 ms, Memory: 40312 KB -BaekjoonHub

--- Java/백준/Silver/21921. 블로그/블로그.java
+++ Java/백준/Silver/21921. 블로그/블로그.java
@@ -1,10 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -13,34 +9,33 @@ public class Main {
         int N = Integer.parseInt(input[0]);
         int X = Integer.parseInt(input[1]);
 
-        String[] visitor = reader.readLine().split(" ");
+        String[] inputVisit = reader.readLine().split(" ");
         int[] visit = new int[N];
         for (int i = 0; i < N; i++) {
-            visit[i] = Integer.parseInt(visitor[i]);
-        }
-        int[] dp = new int[N - X+1];
-        int num = 0;
-        for (int i = 0; i < X; i++) {
-            num += visit[i];
-        }
-        dp[0] = num;
-        for (int i = 1; i <= N - X; i++) {
-            dp[i] = dp[i - 1] - visit[i - 1] + visit[i + X-1];
+            visit[i] = Integer.parseInt(inputVisit[i]);
         }
 
-        int max = 0;
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i <= N - X; i++) {
-            if (dp[i] > max) {
-                max = dp[i];
+        int sum = 0;
+        for (int i = 0; i < X; i++) {
+            sum += visit[i];
+        }
+
+        int max = sum;
+        int cnt = 1;
+        for (int i = 1; i < N - X + 1; i++) {
+            sum += visit[i + X - 1] - visit[i - 1];
+            if (sum > max) {
+                max = sum;
+                cnt = 1;
+            } else if (sum == max) {
+                cnt ++;
             }
-            map.put(dp[i], map.getOrDefault(dp[i], 0) + 1);
         }
         if (max == 0) {
             System.out.println("SAD");
         } else {
             System.out.println(max);
-            System.out.println(map.get(max));
+            System.out.println(cnt);
         }
     }
 }
