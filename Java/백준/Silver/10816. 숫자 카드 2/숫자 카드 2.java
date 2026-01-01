9744cff53c666a74c4feaf44c67cd4d19dbb6834
jaewan96 <139323399+jaewan96@users.noreply.github.com>
2026-01-01 16:01:16 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 976 ms, Memory: 189360 KB -BaekjoonHub

--- Java/백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ Java/백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,29 +1,35 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.StringTokenizer;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        
+
         int N = Integer.parseInt(reader.readLine());
-        HashMap<Integer, Integer> map = new HashMap<>();
-        
-        StringTokenizer first = new StringTokenizer(reader.readLine());
-        for (int i = 0; i < N; i ++) {
-            int card = Integer.parseInt(first.nextToken());
-            map.put(card, map.getOrDefault(card, 0)+1);
+
+        String[] stringCard = reader.readLine().split(" ");
+
+        HashMap<Integer, Integer> card = new HashMap<>();
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(stringCard[i]);
+            card.put(num, card.getOrDefault(num, 0) + 1);
         }
-        
+
         int M = Integer.parseInt(reader.readLine());
-        StringTokenizer sec = new StringTokenizer(reader.readLine());
+
+        String[] checkCard = reader.readLine().split(" ");
+
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i < M; i ++) {
-            int card = Integer.parseInt(sec.nextToken());
-            result.append(map.getOrDefault(card, 0)).append(" ");
+
+        for (int i = 0; i < M; i++) {
+            int num = Integer.parseInt(checkCard[i]);
+
+            result.append(card.getOrDefault(num, 0)).append(" ");
         }
-        System.out.print(result.toString().trim());
+
+        System.out.println(result);
     }
 }
